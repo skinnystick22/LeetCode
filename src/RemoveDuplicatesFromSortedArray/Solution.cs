9a97d3468a32f9d21bdda17b4c22e60712847efa
Tyler Miller <tyler.e.miller1995@gmail.com>
2025-03-10 22:14:07 -0500

    Add solution for "Remove Duplicates from Sorted Array"
    Implemented the solution for the "Remove Duplicates from Sorted Array" problem, including the main logic in `Solution.cs` and corresponding unit tests in a new test project. Updated the solution file to include the new projects and set up necessary dependencies for the test framework.

---
+++ src/RemoveDuplicatesFromSortedArray/Solution.cs
@@ -0,0 +1,20 @@
+﻿namespace RemoveDuplicatesFromSortedArray;
+
+public class Solution
+{
+    public int RemoveDuplicates(int[] nums)
+    {
+        if (nums.Length == 0)
+            return 0;
+
+        var k = 0;
+        for (var i = 1; i < nums.Length; i++)
+        {
+            if (nums[i] != nums[k])
+            {
+                nums[++k] = nums[i];
+            }
+        }
+        return k + 1;
+    }
+}
---
+++ tsts/RemoveDuplicatesFromSortedArrayTest/RemoveDuplicatesFromSortedArrayTests.cs
@@ -0,0 +1,17 @@
+﻿using RemoveDuplicatesFromSortedArray;
+
+namespace RemoveDuplicatesFromSortedArrayTest;
+
+public class RemoveDuplicatesFromSortedArrayTests
+{
+    [Theory]
+    [InlineData(new int[0], 0)]
+    [InlineData(new[] { 1, 1, 2 }, 2)]
+    [InlineData(new[] { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 }, 5)]
+    public void RemoveDuplicateTest(int[] nums, int expectedK)
+    {
+        var solver = new Solution();
+        var result = solver.RemoveDuplicates(nums);
+        Assert.Equal(expectedK, result);
+    }
+}
